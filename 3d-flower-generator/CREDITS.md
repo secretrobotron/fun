e2b3f011ea199c9124893a7dca9c48ea92abcd5f
secretrobotron <secretrobotron@gmail.com>
2017-10-30 11:33:24 -0400

    skybox and transforms

---
+++ 3d-flower-generator/CREDITS.md
@@ -0,0 +1,1 @@
+* Skyboxes by Emil Persson, aka Humus from https://opengameart.org/content/forest-skyboxes and http://www.humus.name
--- 3d-flower-generator/src/js/main.js
+++ 3d-flower-generator/src/js/main.js
@@ -13,21 +13,41 @@ function FlowerGenerator(flowerTemplate, petalTemplate) {
     var petalsContainer = flower.querySelector('.petals');
 
     petals.forEach((petalSettings) => {
-      var numPetals = petalSettings.number || (Math.round(Math.random() * 12) + 4);
-      var colour = petalSettings.colour || defaultColours[Math.floor(Math.random() * defaultColours.length)];
+      var numPetals = petalSettings.number;
+      var colour = petalSettings.colour;
+      var rotation = petalSettings.rotation || 0;
 
       for (var i = 0; i < numPetals; ++i) {
         var petalContainer = petalTemplate.cloneNode(true);
-        petalContainer.style.transform = 'rotate(' + (360/numPetals*i) + 'deg)';
+        var petal = petalContainer.firstElementChild;
+
+        petalContainer.style.transform = 'rotateZ(' + (360/numPetals*i) + 'deg)';
+        petal.style.transform = 'translateZ(-40px) rotateX(' + rotation + 'deg) rotateY(-' + rotation + 'deg)';
+        
+        petal.classList.add(colour);
         petalsContainer.appendChild(petalContainer);
-        petalContainer.firstElementChild.classList.add(colour);
       }
     });
-
 
     flower.classList.add('debug');
 
     return flower;
+  };
+
+  this.createRandomFlower = function(pistilRadius) {
+    var layers = [];
+
+    var numLayers = 4 + Math.round(Math.random() * 8);
+
+    for (var i = 0; i < numLayers; ++i) {
+      layers.push({
+        number: Math.round(Math.random() * 12) + 4,
+        colour: defaultColours[Math.floor(Math.random() * defaultColours.length)],
+        rotation: 15 + Math.round(Math.random() * 15)
+      });
+    }
+
+    return this.createFlower(pistilRadius, layers);
   };
 }
 
@@ -38,18 +58,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   var flowerGenerator = new FlowerGenerator(flowerTemplate, petalTemplate);
   
-  var flower = flowerGenerator.createFlower(100, [
-    {
-      number: 6,
-      colour: 'blue'
-    }
-  ]);
+  var flower = flowerGenerator.createRandomFlower(100);
 
   stage.appendChild(flower);
 
+  flower.style.transform = 'translateX(100px) translateY(100px) translateZ(-700px) rotateX(-10deg)';
+
   document.addEventListener('mousemove', (e) => {
-    var p = e.clientY / window.innerHeight;
-    
-    document.querySelector('.flower-transform').style.transform = 'rotateX(' + (-50*p) + 'deg)';
+    var py = (e.clientY - (window.innerHeight / 2)) / window.innerHeight / 2;
+    var px = (e.clientX - (window.innerWidth / 2)) / window.innerWidth / 2;
+    var skyboxRotationX = py * Math.PI*2;
+    var skyboxRotationY = px * Math.PI*4;
+
+    document.querySelector('.stage-transform.rotation').style.transform ='rotateX(' + skyboxRotationX + 'rad) rotateY(' + skyboxRotationY + 'rad)';
   });
 });
